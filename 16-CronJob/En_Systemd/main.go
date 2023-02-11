8c95948604d2ee03d471b8d2d4572550e3fb3407
Ricardo Antonio Valladares Renderos <R_A_V_R_@HOTMAIL.COM>
2023-02-11 08:33:58 -0600

    Add files via upload

--- 16-CronJob/En_Systemd/main.go
+++ 16-CronJob/En_Systemd/main.go
@@ -2,26 +2,20 @@ package main
 
 import (
 	"fmt"
-	"time"
-	"github.com/go-co-op/gocron"
+	"os"
 )
 
-//tarea
-func tarea(texto string) {
-	fmt.Println(texto)
-}
-
-//cronometrar
-func runCronJobs() {
-	s := gocron.NewScheduler(time.UTC)
-	//s.Every(1).Hours().Do(func() {
-	s.Every(5).Seconds().Do(func() {
-		tarea("Ejecutar...")
- 	})
- 	s.StartBlocking()
-}
-
-//principal
 func main() {
-	runCronJobs()
+	fmt.Println("Ejecutar...")
+	file,err := os.OpenFile("/opt/logs.txt", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+	if err != nil {
+		fmt.Println("No se logro acceder a /opt/logs.txt")
+		return
+	}
+	defer file.Close()
+	_, err2 := file.WriteString("EJECUTADO... \n")
+	if err2 != nil {
+		fmt.Println("No se logro escribir en /opt/logs.txt")
+	}
+	file.Close()
 }
